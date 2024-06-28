bca319e318056c67a3a8c26e30c9c9db8e9e10a6
Hanafi Akmal <hanafiakmal96@gmail.com>
2024-06-28 23:27:54 +0800

    Add another method

--- 106_Python/Q9.py
+++ 106_Python/Q9.py
@@ -2,6 +2,9 @@
 # first n rows of Pascal's triangle. Note : Pascal's triangle is an arithmetic and
 # geometric figure first imagined by Blaise Pascal
 
+'''
+# First method
+
 def print_pascal(n):
     triangle = [[1 for _ in range(i+1)] for i in range(n)]
     for i in range(2, n):
@@ -13,4 +16,38 @@ def print_pascal(n):
     print()
 
 n = int(input("Enter a number to do Pascal's Triangle: "))
+print_pascal(n)
+
+'''
+
+# Second Method
+
+def print_pascal(n):
+    # Initialize Pascal's triangle with appropriate spaces
+    pascal_triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append(pascal_triangle[i - 1][j - 1] + pascal_triangle[i - 1][j])
+        pascal_triangle.append(row)
+    
+    # Find the maximum width of the last row (for formatting)
+    max_width = len(' '.join(map(str, pascal_triangle[-1])))
+    
+    # Print Pascal's triangle with center alignment
+    for row in pascal_triangle:
+        # Calculate leading spaces for centering
+        num_spaces = (max_width - len(' '.join(map(str, row)))) // 2
+        print(' ' * num_spaces, end='')
+        
+        # Print each element of the row
+        for num in row:
+            print(num, end=' ')
+        
+        print()  # Move to the next line
+
+n = int(input("Enter the number of rows: "))
 print_pascal(n)
